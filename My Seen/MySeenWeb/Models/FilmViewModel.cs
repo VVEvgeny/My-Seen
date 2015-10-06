fff8d5822c32ca8ce01790512b0ec78df444e8ce
Evgeny <vvevgeny@gmail.com>
2015-10-06 15:03:22 +0300

    Новая модель

---
+++ My Seen/MySeenWeb/Models/FilmViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+using System.Web.Mvc;
+
+namespace MySeenWeb.Models
+{
+    public class FilmViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        //Foreign key for Standard
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public string Name { get; set; }
+        public DateTime DateSee { get; set; }
+        public int Rate { get; set; }
+        public DateTime DateChange { get; set; }
+
+    }
+}
